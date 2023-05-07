dd065c8bf792752fe5ca6100e3b104b37cc6d242
Nano1412 <nano.khongsri@gmail.com>
2023-05-08 03:12:59 +0700

    add DNAReplication

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,7 +2,8 @@
 
 class Program{
   static void Main(string[] args){
-    PascalTriangle();
+    // PascalTriangle();
+    DNAReplication();
   }
 
 // PascalTriangle-----------------------------------------------------------------
@@ -37,4 +38,64 @@ class Program{
     return factorial;
   }
 // PascalTriangle-END-------------------------------------------------------------
+
+  static void DNAReplication(){
+    bool option;
+    Start:
+    string dna = Console.ReadLine();
+    if(IsValidSequence(dna)){
+      Console.WriteLine("Current half DNA sequence : {0}", dna);
+      Console.Write("Do you want to replicate it ? (Y/N) : ");
+
+      option = OptionSelect("Do you want to replicate it ? (Y/N) : ");
+      if(option){
+        dna = ReplicateSeqeunce(dna);
+        Console.WriteLine("Replicated half DNA sequence : {0}", dna);
+      }
+
+    } else {
+      Console.WriteLine("That half DNA sequence is invalid.");
+    }
+    Console.Write("Do you want to process another sequence ? (Y/N) : ");
+
+    option = OptionSelect("Do you want to process another sequence ? (Y/N) : ");
+    if(option){
+      goto Start;
+    }
+
+  }
+
+  static bool OptionSelect(string repeatWord){
+    while(true){
+      string text = Console.ReadLine();
+      if(text == "Y"){return true;}
+      if(text == "N"){return false;}
+      Console.WriteLine("Please input Y or N.");
+      Console.Write(repeatWord);
+    }
+  }
+
+  static bool IsValidSequence(string halfDNASequence)
+  {
+    foreach(char nucleotide in halfDNASequence)
+    {
+        if(!"ATCG".Contains(nucleotide))
+        {
+            return false;
+        }
+    }
+    return true;
+  }
+
+  static string ReplicateSeqeunce(string halfDNASequence)
+  {
+    string result = "";
+    foreach(char nucleotide in halfDNASequence)
+    {
+        result += "TAGC"["ATCG".IndexOf(nucleotide)];
+    }
+    return result;
+  }
+
+
 }
