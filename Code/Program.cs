1d84af1822ffd39fbae456a3a44b7c1a5532abb0
Nano1412 <nano.khongsri@gmail.com>
2023-05-08 04:09:45 +0700

    add LegendaryMatrix

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,75 +1,94 @@
 ﻿using System;
 
-class Program{
-  static void Main(string[] args){
+class Program
+{
+  static void Main(string[] args)
+  {
     // PascalTriangle();
-    DNAReplication();
+    // DNAReplication();
+    // LegendaryMatrix();
   }
 
-// PascalTriangle-----------------------------------------------------------------
-  static void PascalTriangle(){
+  // PascalTriangle-----------------------------------------------------------------
+  static void PascalTriangle()
+  {
     int row;
 
-    do{
+    do
+    {
       row = int.Parse(Console.ReadLine());
-      if(row >= 0){
+      if (row >= 0)
+      {
         DoPascalTriangle(row);
-      } else {
+      }
+      else
+      {
         Console.WriteLine("Invalid Pascal’s triangle row number.");
       }
     } while (row < 0);
-  } 
+  }
 
-  static void DoPascalTriangle(int row){
-    for(int i = 0; i <= row; i++){
-      for(int j = 0; j <= i; j++){
-        int num = Factorial(i)/(Factorial(j) * Factorial(i - j));
+  static void DoPascalTriangle(int row)
+  {
+    for (int i = 0; i <= row; i++)
+    {
+      for (int j = 0; j <= i; j++)
+      {
+        int num = Factorial(i) / (Factorial(j) * Factorial(i - j));
         Console.Write("{0} ", num);
       }
       Console.WriteLine("");
     }
   }
 
-  static int Factorial(int num){
+  static int Factorial(int num)
+  {
     int factorial = 1;
-    while(num > 0){
+    while (num > 0)
+    {
       factorial *= num--;
     }
     return factorial;
   }
-// PascalTriangle-END-------------------------------------------------------------
+  // PascalTriangle-END-------------------------------------------------------------
 
-  static void DNAReplication(){
-    bool option;
-    Start:
+  // DNAReplication-----------------------------------------------------------------
+  static void DNAReplication()
+  {
+    string repeatWord;
+  Start:
     string dna = Console.ReadLine();
-    if(IsValidSequence(dna)){
+    if (IsValidSequence(dna))
+    {
+      repeatWord = "Do you want to replicate it ? (Y/N) : ";
       Console.WriteLine("Current half DNA sequence : {0}", dna);
-      Console.Write("Do you want to replicate it ? (Y/N) : ");
-
-      option = OptionSelect("Do you want to replicate it ? (Y/N) : ");
-      if(option){
+      Console.Write(repeatWord);
+      if (OptionSelect(repeatWord))
+      {
         dna = ReplicateSeqeunce(dna);
         Console.WriteLine("Replicated half DNA sequence : {0}", dna);
       }
-
-    } else {
+    }
+    else
+    {
       Console.WriteLine("That half DNA sequence is invalid.");
     }
-    Console.Write("Do you want to process another sequence ? (Y/N) : ");
-
-    option = OptionSelect("Do you want to process another sequence ? (Y/N) : ");
-    if(option){
+    repeatWord = "Do you want to process another sequence ? (Y/N) : ";
+    Console.Write(repeatWord);
+    if (OptionSelect(repeatWord))
+    {
       goto Start;
     }
 
   }
 
-  static bool OptionSelect(string repeatWord){
-    while(true){
+  static bool OptionSelect(string repeatWord)
+  {
+    while (true)
+    {
       string text = Console.ReadLine();
-      if(text == "Y"){return true;}
-      if(text == "N"){return false;}
+      if (text == "Y") { return true; }
+      if (text == "N") { return false; }
       Console.WriteLine("Please input Y or N.");
       Console.Write(repeatWord);
     }
@@ -77,12 +96,12 @@ class Program{
 
   static bool IsValidSequence(string halfDNASequence)
   {
-    foreach(char nucleotide in halfDNASequence)
+    foreach (char nucleotide in halfDNASequence)
     {
-        if(!"ATCG".Contains(nucleotide))
-        {
-            return false;
-        }
+      if (!"ATCG".Contains(nucleotide))
+      {
+        return false;
+      }
     }
     return true;
   }
@@ -90,12 +109,79 @@ class Program{
   static string ReplicateSeqeunce(string halfDNASequence)
   {
     string result = "";
-    foreach(char nucleotide in halfDNASequence)
+    foreach (char nucleotide in halfDNASequence)
     {
-        result += "TAGC"["ATCG".IndexOf(nucleotide)];
+      result += "TAGC"["ATCG".IndexOf(nucleotide)];
     }
     return result;
   }
+  // DNAReplication-END-------------------------------------------------------------
 
+  // LegendaryMatrix----------------------------------------------------------------
+
+  static void LegendaryMatrix()
+  {
+  Start:
+    string numOperator = Console.ReadLine();
+    if (numOperator == "+" || numOperator == "-")
+    {
+      int m = int.Parse(Console.ReadLine());
+      int n = int.Parse(Console.ReadLine());
+      float[,] numMatrix_1 = new float[m, n];
+      float[,] numMatrix_2 = new float[m, n];
+      float[,] numMatrix_Ans = new float[m, n];
+
+      numMatrix_1 = MatrixInput(m, n);
+      numMatrix_2 = MatrixInput(m, n);
+      numMatrix_Ans = MatrixOperation(numMatrix_1, numMatrix_2, m, n, numOperator);
+      Print2DMatrix(numMatrix_Ans, m, n);
+      goto Start;
+    }
+  }
+  static float[,] MatrixInput(int m, int n)
+  {
+    float[,] matrix = new float[m, n];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        matrix[i, j] = float.Parse(Console.ReadLine());
+      }
+    }
+    return matrix;
+  }
+  static float[,] MatrixOperation(float[,] num1, float[,] num2, int m, int n, string numOperator)
+  {
+    float[,] ans = new float[m, n];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        switch (numOperator)
+        {
+          case "+":
+            ans[i, j] = num1[i, j] + num2[i, j];
+            break;
+          case "-":
+            ans[i, j] = num1[i, j] - num2[i, j];
+            break;
+        }
+      }
+    }
+    return ans;
+  }
+  static void Print2DMatrix(float[,] matrix, int m, int n)
+  {
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write("{0:.0} ", matrix[i, j]);
+      }
+      Console.WriteLine("");
+    }
+  }
+
+  // LegendaryMatrix-END------------------------------------------------------------
 
 }
